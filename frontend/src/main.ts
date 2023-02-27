cb77a1bd88b48719999cb0f1a8f0f80ddb9c7922
junhaoxu1 <97689187+junhaoxu1@users.noreply.github.com>
2023-02-27 16:02:09 +0100

    Fixing event listener

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,1 +1,29 @@
 import "./assets/scss/style.scss";
+import { io, Socket } from 'socket.io-client'
+import {
+	ClientToServerEvents,
+	ServerToClientEvents,
+} from '@backend/types/shared/SocketTypes'
+
+
+const SOCKET_HOST = import.meta.env.VITE_APP_SOCKET_HOST
+
+const usernameFormEl = document.querySelector('#username-form') as HTMLFormElement
+
+let username: string | null = null
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SOCKET_HOST)
+
+usernameFormEl.addEventListener('submit', e => {
+    e.preventDefault()
+
+    username = (usernameFormEl.querySelector('#username') as HTMLInputElement).value.trim()
+
+    if (!username) {
+        return
+    }
+
+    socket.emit('userJoin', username, () => {
+        console.log('Success?', )
+    })
+})
