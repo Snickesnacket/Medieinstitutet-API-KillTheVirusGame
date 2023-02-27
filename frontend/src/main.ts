524294e435bbe2ec0b3f03941128300137eedf5c
junhaoxu1 <97689187+junhaoxu1@users.noreply.github.com>
2023-02-27 18:50:04 +0100

    Hide login form and show lobby

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,16 +3,25 @@ import { io, Socket } from 'socket.io-client'
 import {
 	ClientToServerEvents,
 	ServerToClientEvents,
-} from '@backend/types/shared/SocketTypes'
+} from '../../backend/src/types/shared/SocketTypes'
 
 
 const SOCKET_HOST = import.meta.env.VITE_APP_SOCKET_HOST
 
 const usernameFormEl = document.querySelector('#username-form') as HTMLFormElement
+const loginEl = document.querySelector('#login') as HTMLDivElement
+const lobbyEl = document.querySelector('#lobby') as HTMLDivElement
 
+let roomId: string | null = null
 let username: string | null = null
 
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SOCKET_HOST)
+
+const showLobby = () => {
+    loginEl.classList.add('hide')
+    console.log("Hello")
+    lobbyEl.classList.remove('hide')
+}
 
 usernameFormEl.addEventListener('submit', e => {
     e.preventDefault()
@@ -20,10 +29,9 @@ usernameFormEl.addEventListener('submit', e => {
     username = (usernameFormEl.querySelector('#username') as HTMLInputElement).value.trim()
 
     if (!username) {
+        console.log("Insert Name")
         return
     }
 
-    socket.emit('userJoin', username, () => {
-        console.log('Success?', )
-    })
+    showLobby()
 })
