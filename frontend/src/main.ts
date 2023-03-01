12d33e99391303734e4af416d69a21cb9883faf4
Astrid Lindfors <astrid.lindfors@gmail.com>
2023-03-01 12:31:13 +0100

    connect user to a room by eventlistener to joinBtn. As of now, the updating of username to display in lobby and room is not working and there is no controll for checking amount of users in the room

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,7 @@ import { io, Socket } from 'socket.io-client'
 import {
 	ClientToServerEvents,
 	ServerToClientEvents,
+    User,
 } from '../../backend/src/types/shared/SocketTypes'
 
 
@@ -12,9 +13,12 @@ const usernameFormEl = document.querySelector('#username-form') as HTMLFormEleme
 const loginEl = document.querySelector('#login') as HTMLDivElement
 const lobbyEl = document.querySelector('#lobby') as HTMLDivElement
 const roomsEl = document.querySelector("#rooms") as HTMLDivElement
+const joinedBtnEl = document.querySelector('#joinBtn') as HTMLButtonElement
+const speldivEl = document.querySelector('#rooms') as HTMLDivElement
 
 let roomId: string | null = null
 let username: string | null = null
+
 
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SOCKET_HOST)
 
@@ -85,4 +89,67 @@ usernameFormEl.addEventListener('submit', e => {
 
         showLobby()
     })
+})
+
+const updateOnlineUsers = (users: User[]) => {
+
+	console.log("updateOnlineUsers:", users)
+
+	roomsEl.innerHTML = users
+		.map(user =>
+			user.id === socket.id
+				? ` <div class="user">${user.name}</div> `
+				:  ` <div class="user">${user.name}</div> `
+		)
+		.join('')
+}
+
+
+
+//TODO: DONT JOIN USER IF ROOM ALREADY HAS 2 USERS! 
+// DONT 
+
+speldivEl.addEventListener('click', e => {
+    const target = e.target as HTMLButtonElement
+
+    if (target.tagName == "BUTTON") {
+         e.preventDefault
+    // Get username
+	roomId = target.value  //(joinedBtnEl.querySelector('#joinBtbn') as HTMLSelectElement).value
+	username = (usernameFormEl.querySelector('#username') as HTMLInputElement).value.trim()
+    console.log( target.tagName)
+
+	// If user or room dosent exit - return. 
+	if (!username || !roomId) {
+		return
+    }
+         loginEl.classList.add('hide')
+         lobbyEl.classList.add('hide')
+   
+
+            socket.emit('userJoin', username, roomId, (result) => {
+            console.log("Join was success?", result)
+
+            if (!result.success || !result.data) {
+                alert("NO ACCESS 4 US")
+                return
+            }
+            updateOnlineUsers(username)
+
+            const roomInfo = result.data
+
+            console.log("roominfo",roomInfo)
+
+            // Update chat view title with room name
+          /*   const chatTitleEl = document.querySelector('#chat-title') as HTMLHeadingElement
+            chatTitleEl.innerText = roomInfo.name */
+
+            // Yay we're allowed to join
+            console.log("Showing chat view")
+            //showChatView() 
+        })
+	/* console.log("Emitted 'userJoin' event to server", username) */
+
+    }
+   
 })
