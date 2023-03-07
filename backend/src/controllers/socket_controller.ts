e6f46c32f762992c3289e9a4a4d4f7ea593565a9
junhaoxu1 <97689187+junhaoxu1@users.noreply.github.com>
2023-03-07 11:47:15 +0100

    Counting current score on each user

--- backend/src/controllers/socket_controller.ts
+++ backend/src/controllers/socket_controller.ts
@@ -111,7 +111,6 @@ export const handleConnection = (socket: Socket<ClientToServerEvents, ServerToCl
             return
         }
 
-
         const user = await prisma.user.upsert({
             where: {
                 id: socket.id
@@ -120,6 +119,7 @@ export const handleConnection = (socket: Socket<ClientToServerEvents, ServerToCl
                 id: socket.id,
                 name: username,
                 speed: 0,
+                score: 0,
                 roomId: "63ff434d4572c0af47e2782b"
             },
             update: {
@@ -170,7 +170,7 @@ export const handleConnection = (socket: Socket<ClientToServerEvents, ServerToCl
                 id: socketId
             },
             data: {
-                speed: reactionTime
+                speed: reactionTime,
             }
         })
 
@@ -189,18 +189,77 @@ export const handleConnection = (socket: Socket<ClientToServerEvents, ServerToCl
             const y = Math.floor(Math.random() * gameBoardSize.y)
             const timeout = Math.floor(Math.random() * (5000 - 1000 * 1) + 1000)
 
-            io.to(user.roomId).emit("updateGame", users, gameRound, timeout, x, y)
-
-            await prisma.user.updateMany({
+            const _user = await prisma.user.update({
                 where: {
-                    roomId: user.roomId
+                  id: socketId,
                 },
                 data: {
-                    speed: 0
-                }
-            })
-        }
-    })
+                  speed: reactionTime,
+                },
+                include: {
+                  room: {
+                    select: {
+                      users: {
+                        select: {
+                          id: true,
+                          speed: true,
+                          score: true,
+                        },
+                      },
+                    },
+                  },
+                },
+              })
+            
+              if (!_user) {
+                return
+              }
+            
+              const roomUsers = _user.room.users
+              console.log(roomUsers)
+              if (roomUsers[1].speed > 0 && roomUsers[0].speed > 0) {
+                const [fastestUser, secondFastestUser] =
+                  roomUsers[1].speed < roomUsers[0].speed
+                    ? [users[0], users[1]]
+                    : [users[1], users[0]]
+
+                    await prisma.user.update({
+                        where: {
+                            id: fastestUser.id,
+                        },
+                        data: {
+                            score: {
+                                increment: fastestUser.id === socketId ? 1 : 0,
+                            },
+                        }
+                    })
+
+                const allUsers = await prisma.user.findMany({
+                    where: {
+                        roomId: user.roomId
+                    }
+                })
+                io.to(user.roomId).emit("updateGame", allUsers, gameRound, timeout, x, y)
+          
+                await prisma.user.updateMany({
+                  where: {
+                    roomId: user.roomId,
+                  },
+                  data: {
+                    speed: 0,
+                  },
+                })
+              }
+
+              const updatedRoomUsers = await prisma.user.findMany({
+                where: {
+                  roomId: user.roomId,
+                },
+              })
+              console.log(updatedRoomUsers)
+            }
+        })
+
 
     socket.on("gameOver", async (socketId) => {
         // Update user.roomId to the lobbyId & speed to 0
@@ -210,6 +269,7 @@ export const handleConnection = (socket: Socket<ClientToServerEvents, ServerToCl
             }, 
             data: {
                 speed: 0,
+                score: 0,
                 roomId: "63ff434d4572c0af47e2782b"
             }
         })
@@ -218,3 +278,4 @@ export const handleConnection = (socket: Socket<ClientToServerEvents, ServerToCl
         socket.join("63ff434d4572c0af47e2782b")
     })
 }
+
