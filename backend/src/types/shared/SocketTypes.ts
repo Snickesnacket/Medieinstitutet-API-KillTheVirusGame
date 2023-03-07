e1c54545a02f720ed0544a3dc6fdd89aad62c30e
junhaoxu1 <97689187+junhaoxu1@users.noreply.github.com>
2023-03-07 15:50:47 +0100

    Prettify + removed unused variables

--- backend/src/types/shared/SocketTypes.ts
+++ backend/src/types/shared/SocketTypes.ts
@@ -1,38 +1,46 @@
-import { Room, User } from '@prisma/client'
-export {}
+import { Room, User } from "@prisma/client";
+export {};
 
 export interface ServerToClientEvents {
-    userJoined: (user: User[]) => void
-    startGame: (timeout: number, x: number, y: number) => void
-    waitingForPlayers: (users: User[]) => void
-    updateGame: (users: User[], newGameRound: number, timeout: number, x: number, y: number) => void
-    userNames: (users: User[]) => void
+  userJoined: (user: User[]) => void;
+  startGame: (timeout: number, x: number, y: number) => void;
+  waitingForPlayers: (users: User[]) => void;
+  updateGame: (
+    users: User[],
+    newGameRound: number,
+    timeout: number,
+    x: number,
+    y: number
+  ) => void;
+  userNames: (users: User[]) => void;
 }
 
 export interface ClientToServerEvents {
-    getRoomList: (callback: (rooms: RoomInfoData[]) => void) => void
-    userJoin: (gameBoardSize: {x: number, y: number}, username: string, roomId: string, callback: (result: UserJoinResult) => void) => void
-    createUser: (username: string, callback: (result: UserJoinResult) => void) => void
-    virusClicked: (gameBoardSize: {x: number, y: number}, gameRound: number, reactionTime: number, socketId: string) => void
-    gameOver: (socketId: string) => void
-}
-
-export interface InterServerEvents {
-}
-
-export interface NoticeData {
-
-}
-
-export interface InterServerEvents {
-
+  getRoomList: (callback: (rooms: RoomInfoData[]) => void) => void;
+  userJoin: (
+    gameBoardSize: { x: number; y: number },
+    username: string,
+    roomId: string,
+    callback: (result: UserJoinResult) => void
+  ) => void;
+  createUser: (
+    username: string,
+    callback: (result: UserJoinResult) => void
+  ) => void;
+  virusClicked: (
+    gameBoardSize: { x: number; y: number },
+    gameRound: number,
+    reactionTime: number,
+    socketId: string
+  ) => void;
+  gameOver: (socketId: string) => void;
 }
 
 export interface RoomInfoData extends Room {
-    users: User[]
+  users: User[];
 }
 
 export interface UserJoinResult {
-	success: boolean
-	data: RoomInfoData | null
+  success: boolean;
+  data: RoomInfoData | null;
 }
